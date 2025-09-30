e9a99216d7ffc82aab61ba4e1d2848fea8c16b4b
22catch22 <114041834+22catch22@users.noreply.github.com>
2025-09-30 12:16:43 -0700

    Update vite.config.js

--- test3/vite.config.js
+++ test3/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://22catch22.github.io/Test3/",
+  base: "/Test3",
 })
